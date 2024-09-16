dc0ae2251ee6e39da8f4dcebdef3360b3e9bcd72
Nicolas Médoc <nicolas.medoc@list.lu>
2024-09-16 16:16:21 +0200

    add getDefaultFontSize()

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,19 +1,19 @@
-function generateValue(value,i){
+function generateValue(typeGen,i){
     let effectiveValue=null;
-    if (value==="random"){
+    if (typeGen==="random"){
         effectiveValue=Math.random();
-    }else if(value==="increment"){
+    }else if(typeGen==="increment"){
         effectiveValue=i;
     }
     return effectiveValue;
 }
-export function getGridData(nbRows, nbColumns, typeGen=null, typeGen2=null){
+export function genGridData(nbRows, nbColumns, typeGen="random", typeGen2="random"){
     const valuesArr = []
     for(let i=0;i<nbRows*nbColumns;i++){
         let effectiveValue=generateValue(typeGen,i);
         let effectiveValue2 = generateValue(typeGen2,i);
 
-        const cellObj = {index:i, x:i%nbColumns, y:Math.floor(i/nbColumns), value:effectiveValue, value2:effectiveValue2}
+        const cellObj = {index:i, rowPos:Math.floor(i/nbColumns), colPos:i%nbColumns, value:effectiveValue, value2:effectiveValue2}
         valuesArr.push(cellObj)
     }
     return valuesArr;
@@ -26,3 +26,24 @@ export function getBlueHue(){
 export function getYlGnBu(){
     return ['#ffffd9','#edf8b1','#c7e9b4','#7fcdbb','#41b6c4','#1d91c0','#225ea8','#253494','#081d58']
 }
+
+export function getDefaultFontSize (){
+    const element = document.createElement('div');
+    element.style.width = '1rem';
+    element.style.display = 'none';
+    document.body.append(element);
+
+    const widthMatch = window
+        .getComputedStyle(element)
+        .getPropertyValue('width')
+        .match(/\d+/);
+
+    element.remove();
+
+    if (!widthMatch || widthMatch.length < 1) {
+        return null;
+    }
+
+    const result = Number(widthMatch[0]);
+    return !isNaN(result) ? result : null;
+};
